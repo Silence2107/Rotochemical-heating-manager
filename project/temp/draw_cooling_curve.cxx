cec4a2ca8201e0e72ab00f5a249e0b2262166763
Silence2107 <pashka1201@gmail.com>
2023-04-16 22:04:58 +0200

    Standalone applications for M-R diagram and cooling curve

---
+++ project/temp/draw_cooling_curve.cxx
@@ -0,0 +1,239 @@
+
+#include "../include/auxiliaries.h"
+#include "../include/cooling.h"
+#include "../include/eos_reader.h"
+#include "../include/constants.h"
+#include "../include/tov_solver.h"
+#include "../include/inputfile.hpp"
+
+#include <vector>
+#include <functional>
+#include <cmath>
+#include <stdexcept>
+#include <iostream>
+
+#include <sstream>
+#include <fstream>
+
+#include <TCanvas.h>
+#include <TGraph.h>
+#include <TAxis.h>
+#include <TLegend.h>
+
+int main()
+{
+    using namespace inputfile;
+
+    // RUN --------------------------------------------------------------------------
+
+    // EoS definition
+
+    auto eos_cached = auxiliaries::CachedFunc<std::vector<std::vector<double>>, double, double>(
+        [&](std::vector<std::vector<double>> &cache, double rho)
+        {
+            if (rho < 0 || rho > edensity_upp * energy_density_conversion)
+                throw std::runtime_error("Data request out of range; Encountered in main::eos_cached");
+            if (rho <= edensity_low * energy_density_conversion)
+                return 0.0;
+            if (cache.empty() || cache[0].size() != discr_size_EoS)
+            {                                                                                        // then fill/refill cache
+                cache = std::vector<std::vector<double>>(2, std::vector<double>(discr_size_EoS, 0)); // initialize 2xdiscr_size_EoS matrix
+                std::vector<double> x(discr_size_EoS, 0);
+                for (int i = 1; i < discr_size_EoS - 1; ++i)
+                { // cache EoS for further efficiency
+                    x[i] = i * (nbar_upp - nbar_low) / discr_size_EoS + nbar_low;
+                    cache[0][i] = energy_density_conversion * energy_density_of_nbar(x[i]);
+                    cache[1][i] = pressure_conversion * pressure_of_nbar(x[i]);
+                }
+                x[0] = nbar_low;
+                x[x.size() - 1] = nbar_upp;
+                cache[0][0] = energy_density_conversion * edensity_low;
+                cache[0][cache[0].size() - 1] = energy_density_conversion * edensity_upp;
+                cache[1][0] = pressure_conversion * pressure_low;
+                cache[1][cache[1].size() - 1] = pressure_conversion * pressure_upp;
+                eos_interpolator_cached.erase(); // clean up cached interpolator
+            }
+            return eos_interpolator(cache[0], cache[1], rho);
+        });
+
+    // TOV solver
+
+    auto tov_cached = auxiliaries::CachedFunc<std::vector<std::vector<double>>, std::vector<double>,
+                                              const std::function<double(double)> &, double, double, double, double>(tov_solver::tov_solution);
+    auto tov = [&tov_cached, &eos_cached](double r)
+    {
+        // TOV solution cached
+        return tov_cached(eos_cached, r, center_density, radius_step, density_step);
+    };
+
+    auto nbar = auxiliaries::CachedFunc<std::vector<std::vector<double>>, double, double>(
+        [&](std::vector<std::vector<double>> &cache, double r)
+        {
+            // cache contains {r, n_B(r)} arrays; recaching is not supported at the moment, call ::erase instead
+            // return nbar(r) for given r (in datafile units)
+
+            if (cache.empty())
+            {
+                nbar_interpolator_cached.erase(); // clean up cached interpolator
+                double R_ns = tov(0.0)[4];
+                cache = std::vector<std::vector<double>>(2, std::vector<double>());
+                for (double r_current = 0; r_current < R_ns; r_current += radius_step)
+                    cache[0].push_back(r_current);
+                for (size_t i = 0; i < cache[0].size(); ++i)
+                {
+                    double r_current = cache[0][i];
+                    // now we somehow have to find corresponding n_B
+                    // let's stick to densities
+                    double density_at_r = tov(r_current)[1];
+
+                    double nbar_left = nbar_low, nbar_right = nbar_upp; // we need these for bisection search; in fm-3 units for now
+                    double nbar_mid = (nbar_left + nbar_right) / 2.0;
+                    while (fabs(nbar_right - nbar_left) > nbar_low)
+                    {
+                        // while we are too far from appropriate precision for nbar estimate
+                        // recalculate via bisection method
+                        nbar_mid = (nbar_left + nbar_right) / 2.0;
+                        if (energy_density_conversion * energy_density_of_nbar(nbar_mid) > density_at_r)
+                            nbar_right = nbar_mid;
+                        else
+                            nbar_left = nbar_mid;
+                    }
+                    cache[1].push_back(nbar_mid);
+                }
+                cache[0].push_back(R_ns);
+                cache[1].push_back(0.0);
+            }
+            return nbar_interpolator(cache[0], cache[1], r);
+        });
+
+    // Cooling
+
+    double r_ns = tov(0.0)[4];
+    double m_ns = tov(r_ns)[0];
+
+    auto exp_phi = [&tov](double r)
+    {
+        return std::exp(tov(r)[2]);
+    };
+
+    auto exp_lambda = [&tov](double r)
+    {
+        return pow(1 - 2 * constants::scientific::G * tov(r)[0] / r, -0.5);
+    };
+
+    // photon luminosity
+    auto photon_luminosity = cooling::predefined::photonic::surface_luminosity(r_ns, m_ns, crust_eta);
+
+    // neutrino luminosity
+    auto hadron_durca_emissivity = cooling::predefined::neutrinic::hadron_durca_emissivity(
+        k_fermi_of_nbar, m_stars_of_nbar, nbar, nbar_core_limit, exp_phi, superfluid_n_1s0,
+        superfluid_p_1s0, superfluid_n_3p2, superfluid_p_temp, superfluid_n_temp);
+
+    auto hadron_murca_emissivity = cooling::predefined::neutrinic::hadron_murca_emissivity(
+        k_fermi_of_nbar, m_stars_of_nbar, nbar, nbar_core_limit, nbar_conversion, exp_phi, superfluid_n_1s0,
+        superfluid_p_1s0, superfluid_n_3p2, superfluid_p_temp, superfluid_n_temp);
+
+    auto hadron_bremsstrahlung_emissivity = cooling::predefined::neutrinic::hadron_bremsstrahlung_emissivity(
+        k_fermi_of_nbar, m_stars_of_nbar, nbar, ion_volume_fr, nbar_core_limit, exp_phi, superfluid_n_1s0,
+        superfluid_p_1s0, superfluid_n_3p2, superfluid_p_temp, superfluid_n_temp);
+
+    auto hadron_PBF_emissivity = cooling::predefined::neutrinic::hadron_pbf_emissivity(
+        k_fermi_of_nbar, m_stars_of_nbar, nbar, nbar_core_limit, exp_phi, superfluid_n_1s0,
+        superfluid_p_1s0, superfluid_n_3p2, superfluid_p_temp, superfluid_n_temp);
+
+    auto Q_nu = [&](double r, double t, double T)
+    {
+        using namespace constants::scientific;
+        double result = 0;
+        for (auto it = m_stars_of_nbar.begin(); it != m_stars_of_nbar.end(); ++it)
+        {
+            auto key = it->first;
+            if (key.classify() == auxiliaries::Species::ParticleClassification::kLepton)
+            {
+                result += hadron_murca_emissivity(r, key, t, T);
+                result += hadron_durca_emissivity(r, key, t, T);
+            }
+            if (key.classify() == auxiliaries::Species::ParticleClassification::kBaryon)
+            {
+                result += hadron_PBF_emissivity(r, key, t, T);
+            }
+        }
+        result += hadron_bremsstrahlung_emissivity(r, t, T);
+        return result * exp_phi(r) * exp_phi(r);
+    };
+
+    auto neutrino_luminosity = auxiliaries::integrate_volume<double, double>(
+        std::function<double(double, double, double)>(Q_nu), 0, r_ns, exp_lambda, auxiliaries::IntegrationMode::kGaussLegendre_12p, radius_step);
+
+    // specific heat
+    auto fermi_specific_heat_dens = cooling::predefined::auxiliary::fermi_specific_heat_density(
+        k_fermi_of_nbar, m_stars_of_nbar, nbar, nbar_core_limit, exp_phi, superfluid_n_1s0,
+        superfluid_p_1s0, superfluid_n_3p2, superfluid_p_temp, superfluid_n_temp);
+
+    auto heat_capacity = auxiliaries::integrate_volume<double, double>(
+        std::function<double(double, double, double)>(fermi_specific_heat_dens), 0, r_ns, exp_lambda, auxiliaries::IntegrationMode::kGaussLegendre_12p, radius_step);
+
+    auto cooling_rhs = [&heat_capacity, &photon_luminosity, &neutrino_luminosity](double t, double T)
+    {
+        // std::cout << t << " " << photon_luminosity(t, T) << " " << neutrino_luminosity(t, T) << " " << heat_capacity(t, T) << '\n';
+        return -(photon_luminosity(t + t_init, T) + neutrino_luminosity(t + t_init, T)) / heat_capacity(t + t_init, T);
+    };
+
+    // solve cooling equation
+
+    auto cooling_solver = auxiliaries::CachedFunc<std::vector<std::vector<double>>, double, double, const std::function<double(double, double)> &, double, double, double,
+                                                  const std::function<double(const std::vector<double> &, const std::vector<double> &, double)> &>(cooling::solver::stationary_cooling_cached);
+
+    double exp_phi_at_R = pow(1 - 2 * constants::scientific::G * m_ns / r_ns, 0.5);
+
+    double T_init = T_init_local * exp_phi_at_R;
+
+    // invoke the solver once to cache the solution
+    cooling_solver(t_end - t_init, cooling_rhs, T_init, base_t_step, exp_rate_estim, cooling_interpolator);
+
+    // plot the solution
+    std::vector<double> x(cooling_n_points_estimate, 0);
+    std::vector<double> y(cooling_n_points_estimate, 0);
+    std::cout << "M/Msol " << m_ns * constants::conversion::gev_over_msol << std::endl;
+    std::cout << "t [years] "
+              << "\tTe^inf [K] "
+              << "\tL_ph [erg/s] "
+              << "\tL_nu [erg/s] " << std::endl;
+    for (int i = 0;; ++i)
+    {
+        x[i] = base_t_step * (pow(exp_rate_estim, i + 1) - 1) / (exp_rate_estim - 1);
+        if (x[i] > t_end)
+        {
+            x.resize(i);
+            y.resize(i);
+            break;
+        }
+        y[i] = cooling_solver(x[i], cooling_rhs, T_init, base_t_step, exp_rate_estim, cooling_interpolator);
+        // print in understandable units
+        std::cout << 1.0E6 * (x[i] + t_init) / (constants::conversion::myr_over_s * constants::conversion::gev_s) << "\t" << cooling::predefined::auxiliary::te_tb_relation(y[i], r_ns, m_ns, crust_eta) * exp_phi_at_R * constants::conversion::gev_over_k << "\t" << photon_luminosity(x[i], y[i]) * constants::conversion::gev_s / constants::conversion::erg_over_gev << "\t" << neutrino_luminosity(x[i], y[i]) * constants::conversion::gev_s / constants::conversion::erg_over_gev << "\t" << '\n';
+        // rescale
+        x[i] += t_init;
+        x[i] *= 1.0E6 / (constants::conversion::myr_over_s * constants::conversion::gev_s);
+        y[i] = cooling::predefined::auxiliary::te_tb_relation(y[i], r_ns, m_ns, crust_eta) * exp_phi_at_R * constants::conversion::gev_over_k;
+    }
+
+    //draw
+    TCanvas *c1 = new TCanvas("c1", "c1");
+    auto gr = new TGraph(x.size(), x.data(), y.data());
+    gr->SetLineColor(kBlue);
+    gr->Draw("AL");
+    // title offset
+    gr->GetYaxis()->SetTitleOffset(1.5);
+    gPad->SetLogx();
+    gPad->SetLogy();
+
+    gr->GetXaxis()->SetTitle("t [yr]");
+    gr->GetYaxis()->SetTitle("T [K]");
+
+    auto legend = new TLegend(0.1, 0.1, 0.38, 0.38);
+    legend->AddEntry(gr, "RH Manager", "l");
+
+    legend->Draw();
+
+    c1->SaveAs("cooling.pdf");
+}
