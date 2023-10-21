8ce560e05fbf1a2da7b21957f49c552bd3e5159d
Silence2107 <pashka1201@gmail.com>
2023-10-21 17:39:21 +0200

    Acknowledge 3rd party

--- 3rd-party/argparse/argparse.hpp
+++ 3rd-party/argparse/argparse.hpp
@@ -144,6 +144,15 @@ namespace argparse {
             buf = std::stoull(_arguments.at(kw)[index]);
             return true;
         }
+        // BY PAVLO PANASIUK
+        bool get(const std::string &kw, double &buf, std::size_t index = 0) const {
+            if (_arguments.count(kw) == 0)
+                return false;
+            if (_arguments.at(kw).size() <= index)
+                return false;
+            buf = std::stod(_arguments.at(kw)[index]);
+            return true;
+        }
         bool get(const std::string &kw, std::string &buf, std::size_t index = 0) const {
             if (_arguments.count(kw) == 0)
                 return false;
