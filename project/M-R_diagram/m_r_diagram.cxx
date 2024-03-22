d5f16e5ae8f32104aa0379b19cbb7e070fcf3c5d
Silence2107 <pashka1201@gmail.com>
2024-03-23 00:50:00 +0100

    appropriate rename

--- project/M-R_diagram/m_r_diagram.cxx
+++ project/M-R_diagram/m_r_diagram.cxx
@@ -26,7 +26,7 @@
 
 int main(int argc, char **argv)
 {
-    argparse::ArgumentParser parser("m_r_diagram", "Maps central densities selection to corresponding mass and radius based on EoS", "Argparse powered by SiLeader");
+    argparse::ArgumentParser parser("m_r_diagram_inversed_eos", "Maps central pressures selection to corresponding mass and radius based on EoS", "Argparse powered by SiLeader");
 
 #if RHM_REQUIRES_INPUTFILE
     parser.addArgument({"--inputfile"}, "json input file path (required)");
@@ -35,9 +35,9 @@ int main(int argc, char **argv)
     parser.addArgument({"--pdf_path"}, "pdf output file path (optional, default: M-R-diagram.pdf)");
     parser.addArgument({"--rootfile_path"}, "root output file path (optional, default: None)");
 #endif
-    parser.addArgument({"--left_fraction"}, "least fraction of central density to consider (optional, default: 0.001)");
-    parser.addArgument({"--right_fraction"}, "greatest fraction of central density to consider (optional, default: 0.999)");
-    parser.addArgument({"--selection_size"}, "number of points to discretize density interval (optional, default: 1000)");
+    parser.addArgument({"--left_fraction"}, "least fraction of central pressure to consider (optional, default: 0.001)");
+    parser.addArgument({"--right_fraction"}, "greatest fraction of central pressure to consider (optional, default: 0.999)");
+    parser.addArgument({"--selection_size"}, "number of points to discretize pressure interval (optional, default: 1000)");
     auto args = parser.parseArgs(argc, argv);
 
     using namespace instantiator;
@@ -59,11 +59,11 @@ int main(int argc, char **argv)
     // RUN --------------------------------------------------------------------------
 
     // EoS definition
-
-    auto eos_cached = auxiliaries::math::CachedFunc<std::vector<std::vector<double>>, double, double>(
-        [&](std::vector<std::vector<double>> &cache, double rho)
+    
+    auto eos_inv_cached = auxiliaries::math::CachedFunc<std::vector<std::vector<double>>, double, double>(
+        [&](std::vector<std::vector<double>> &cache, double p)
         {
-            if (rho < edensity_low || rho > edensity_upp)
+            if (p < pressure_low || p > pressure_upp)
                 RHM_THROW(std::runtime_error, "Data request out of range.");
             if (cache.empty() || cache[0].size() != discr_size_EoS)
             {                                                                                        // then fill/refill cache
@@ -72,30 +72,30 @@ int main(int argc, char **argv)
                 for (int i = 1; i < discr_size_EoS - 1; ++i)
                 { // cache EoS for further efficiency
                     x[i] = i * (nbar_upp - nbar_low) / discr_size_EoS + nbar_low;
-                    cache[0][i] = energy_density_of_nbar(x[i]);
-                    cache[1][i] = pressure_of_nbar(x[i]);
+                    cache[0][i] = pressure_of_nbar(x[i]);
+                    cache[1][i] = energy_density_of_nbar(x[i]);
                 }
                 x[0] = nbar_low;
                 x[x.size() - 1] = nbar_upp;
-                cache[0][0] = edensity_low;
-                cache[0][cache[0].size() - 1] = edensity_upp;
-                cache[1][0] = pressure_low;
-                cache[1][cache[1].size() - 1] = pressure_upp;
+                cache[0][0] = pressure_low;
+                cache[0][cache[1].size() - 1] = pressure_upp;
+                cache[1][0] = edensity_low;
+                cache[1][cache[0].size() - 1] = edensity_upp;
                 eos_interpolator_cached.erase(); // clean up cached interpolator
             }
-            return eos_interpolator(cache[0], cache[1], rho);
+            return eos_interpolator(cache[0], cache[1], p);
         });
 
-    // returns {r, m} pair at given center density. (Hopefully) cleans up all global cache that may spoil further calls
-    auto get_m_r_at_density = [&](double edensity)
+    // returns {r, m} pair at given center pressure. (Hopefully) cleans up all global cache that may spoil further calls
+    auto get_m_r_at_pressure = [&](double pressure)
     {
         // TOV solver
         auto tov_cached = auxiliaries::math::CachedFunc<std::vector<std::vector<double>>, std::vector<double>,
-                                                        const std::function<double(double)> &, double, double, double, double, size_t>(tov_solver::tov_solution);
-        auto tov = [&tov_cached, &eos_cached, edensity](double r)
+                                                        const std::function<double(double)> &, double, double, double, double, size_t>(tov_solver::tov_solution_inverse_eos);
+        auto tov = [&tov_cached, &eos_inv_cached, pressure](double r)
         {
             // TOV solution cached
-            return tov_cached(eos_cached, r, edensity, radius_step, surface_pressure, tov_adapt_limit);
+            return tov_cached(eos_inv_cached, r, pressure, radius_step, surface_pressure, tov_adapt_limit);
         };
 
         double r_ns = tov(0.0)[4];
@@ -109,17 +109,17 @@ int main(int argc, char **argv)
     size_t indent = 20;
     std::vector<double> x, y, z;
     // assemble data for different center densities
-    std::cout << std::left << std::setw(indent) << "rho fraction" << std::setw(indent) << "rho [df. units]" << std::setw(indent) << "M [Ms]" << std::setw(indent) << "R [km]" << '\n';
+    std::cout << std::left << std::setw(indent) << "pressure fraction" << std::setw(indent) << "pressure [df. units]" << std::setw(indent) << "M [Ms]" << std::setw(indent) << "R [km]" << '\n';
     for (size_t count = 0; count < selection_size; ++count)
     {
         using namespace constants::conversion;
         double frac = left_fraction + count * (right_fraction - left_fraction) / (selection_size - 1);
-        double edensity = frac * (edensity_upp - edensity_low) + edensity_low;
-        auto point = get_m_r_at_density(edensity);
+        double pressure = frac * (pressure_upp - pressure_low) + pressure_low;
+        auto point = get_m_r_at_pressure(pressure);
         x.push_back(point[0] / km_gev);
         y.push_back(point[1] * gev_over_msol);
-        z.push_back(edensity / energy_density_conversion);
-        std::cout << std::left << std::setw(indent) << frac << std::setw(indent) << edensity / energy_density_conversion << std::setw(indent) << y.back() << std::setw(indent) << x.back() << "\n";
+        z.push_back(pressure / pressure_conversion);
+        std::cout << std::left << std::setw(indent) << frac << std::setw(indent) << z.back() << std::setw(indent) << y.back() << std::setw(indent) << x.back() << "\n";
     }
 
 #if RHM_HAS_ROOT
@@ -131,16 +131,16 @@ int main(int argc, char **argv)
     gr->GetYaxis()->SetTitle("M [Ms]");
     if (rootfile)
     {
-        auto gr_rho = new TGraph(z.size(), z.data(), x.data());
-        gr_rho->GetXaxis()->SetTitle("#rho [datafile units]");
-        gr_rho->GetYaxis()->SetTitle("R [km]");
+        auto gr_p = new TGraph(z.size(), z.data(), x.data());
+        gr_p->GetXaxis()->SetTitle("P [datafile units]");
+        gr_p->GetYaxis()->SetTitle("R [km]");
         auto gr_m = new TGraph(z.size(), z.data(), y.data());
-        gr_m->GetXaxis()->SetTitle("#rho [datafile units]");
+        gr_m->GetXaxis()->SetTitle("P [datafile units]");
         gr_m->GetYaxis()->SetTitle("M [Ms]");
         rootfile->cd();
         rootfile->WriteObject(gr, "m_r_diagram");
-        rootfile->WriteObject(gr_rho, "rho_r_diagram");
-        rootfile->WriteObject(gr_m, "m_rho_diagram");
+        rootfile->WriteObject(gr_p, "r_p_diagram");
+        rootfile->WriteObject(gr_m, "m_p_diagram");
         rootfile->Close();
     }
     gr->SetLineColor(kBlue);
